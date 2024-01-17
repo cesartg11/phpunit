ae559c322e85b36b304672bcc7360762f3676f09
cesartg11 <cesar.torregonzalez@iesmiguelherrero.com>
2024-01-17 17:35:10 +0100

    base de datos falta

---
+++ database/migrations/2024_01_17_162857_create_animales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animales', function (Blueprint $table) {
+            $table->id();
+            $table->string('especie')->unique();
+            $table->string('slug')->unique();
+            $table->double('peso');
+            $table->double('altura');
+            $table->date('fechaNacimiento');
+            $table->string('fechaNacimiento')->nullable();
+            $table->string('imagen')->nullable();
+            $table->string('imagen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animales');
+    }
+};
